e7e762105bfc41a7e8069941dfbdd02577b30713
mparadas <mparadas@gmail.com>
2026-02-27 17:10:30 -0400

    Add files via upload

---
+++ src/api-config.js
@@ -0,0 +1,6 @@
+// CONFIGURACIÓN SIMPLE Y DIRECTA
+export const API_URL = 'https://agromae.onrender.com/api'
+export const IMAGES_URL = 'https://agromae.onrender.com/uploads'
+
+console.log('🔥 API URL DIRECTA:', API_URL)
+console.log('🖼️ IMAGES URL DIRECTA:', IMAGES_URL)
