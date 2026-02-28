afe270c5846e4457778681a59738c97b34ccd0aa
mparadas <mparadas@gmail.com>
2026-02-28 10:48:04 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function Layout({ children, usuario, onLogout }) {
         <div className="h-full flex flex-col">
           <div className="p-4 border-b bg-transparent flex items-center justify-center">
             <img
-              src="/IMG-20260227-WA0022.jpg"
+              src="/IMG_9805.png"
               alt="AgroMAE"
               className="h-14 w-auto object-contain bg-transparent"
               style={{ mixBlendMode: 'darken', backgroundColor: 'transparent' }}
@@ -150,7 +150,7 @@ function Layout({ children, usuario, onLogout }) {
                 <Menu className="w-6 h-6" />
               </button>
               <img
-                src="/IMG-20260227-WA0022.jpg"
+                src="/IMG_9805.png"
                 alt="AgroMAE"
                 className="ml-3 h-8 w-auto object-contain bg-transparent"
                 style={{ mixBlendMode: 'darken', backgroundColor: 'transparent' }}
@@ -213,7 +213,7 @@ function Login({ onLogin }) {
       <div className="absolute top-1/3 -right-20 w-80 h-80 bg-amber-200/30 rounded-full blur-3xl pointer-events-none" />
       <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
         <img
-          src="/IMG-20260227-WA0022.jpg"
+          src="/IMG_9805.png"
           alt="Marca de agua AgroMAE"
           className="w-[70vw] max-w-[760px] opacity-[0.08] object-contain bg-transparent"
           style={{ mixBlendMode: 'darken', backgroundColor: 'transparent' }}
@@ -224,7 +224,7 @@ function Login({ onLogin }) {
         <div className="flex flex-col lg:flex-row lg:items-start lg:justify-between gap-6">
           <div className="flex-1">
             <img
-              src="/IMG-20260227-WA0022.jpg"
+              src="/IMG_9805.png"
               alt="AgroMAE"
               className="h-16 sm:h-20 w-auto object-contain mb-3 bg-transparent"
               style={{ mixBlendMode: 'darken', backgroundColor: 'transparent' }}
